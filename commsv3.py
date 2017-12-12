25895f2410300a33790bc598a0e2320e6932c55b
Jackson Lohman <jacksonlohman@gmail.com>
2017-12-12 16:16:50 -0700

    Fixed: PWM direction, cam, syntax errors

--- commsv3.py
+++ commsv3.py
@@ -1,12 +1,14 @@
 #Made for Python 3, By Jackson Lohman and TJ Reynolds
 #To be run by a Raspberry Pi on an RC car
 try:
-	from picamera import PiCamera
+	from picamera import PiCamera as picam
 except ModuleNotFoundError:
 	print('PiCamera Disabled')
 	camSupport = False
 else:
-	picam.resolution = (320,320)
+	camSupport = True
+	picam = PiCamera()
+	picam.resolution = (320,320)#everything else runs and gives this time to setup
 from PIL import Image
 import numpy as np
 import threading
@@ -18,10 +20,11 @@ import os
 
 #Global Vars to change when running (change)
 tfIpAddress = '192.168.0.11'#Where tensorflow is on the local network
-
+arduinoDistance = False
 
 #program  global vars (do not change)
 imgCounter = 0#pictures taken
+ser = ''
 
 def inputScrubber(inputStr, optionTuple, errorStr):#make sure optionTuple is str, not int
 	responce = input(inputStr)
@@ -43,10 +46,11 @@ def inputScrubber(inputStr, optionTuple, errorStr):#make sure optionTuple is str
 ###########################
 
 def bluescale():
-	global imgCounter
-	currentImg = '/home/pi/Documents/running/run' +str(imgCounter) + '.jpg'
+	global imgCounter, camSupport
+	currentImg = '/home/pi/Documents/TFtrain/run/img' +str(imgCounter) + '.jpg'
 	if camSupport == True:
 		picam.capture(currentImg)
+		time.sleep(10)
 		print('Image :'+str(imgCounter)+' taken.')
 	else:
 		print('Using a local image')
@@ -68,6 +72,7 @@ def sendImg(image):
 		sock.connect(server_address)
 	except OSError:
 		print('Could not send image to Tensorflow')
+		time.sleep(.1)
 	else:
 		print('Image sent to Tensorflow')
 
@@ -111,48 +116,59 @@ def brain(command, distance):#optimize values when it is working
 		print('Moving Forward...')
 		return pwmA, pwmB
 	elif command == 2:#turn left
-		pwmA = 255
-		pwmB = -255
+		pwmA = 0#HERE
+		pwmB = 255#HERE
 		print('Turning Left...')
 		return pwmA, pwmB
 	elif command == 3:#turn right
-		pwmA = -255
-		pwmB = 255
+		pwmA = 255#HERE
+		pwmB = 0#HERE
 		print('Turning Right...')
 		return pwmA, pwmB
 	else:#this should not run
 		print('No valid command in brain()\nExiting...')
 		exit()
 
-def arduino(command):
+def connectArduinoSer():
+	global ser
 	counter = 0
 	while counter <= 10:#loop through possible locations
 		try:
 			ser = serial.Serial('/dev/ttyACM'+str(counter), 9600)
 		except OSError: #OSError:#if it cannot find the filepath
 			counter += 1
+			arduinoFoundSer = False
 		else:#runs if there are no exceptions in the try
 			time.sleep(1)
+			arduinoFoundSer = True
 			print('Arduino is at: /dev/ttyACM'+str(counter))
-			counter = 22#exit out of the loop
-	if counter == 21:
+			counter = 11#exit out of the loop
+	if arduinoFoundSer == False:
 		print('Arduino not found\n  Will try again in 3s')#when the while loop is over
 		time.sleep(3)
+		connectArduino(command)
+
+
+def arduino(command):
 	x = 0
-	while x < 5:#get distance
-		raw = ser.readline()
-		try:#if bits are transmitted incorrectly, it ignores it
-			decoded = raw.decode().strip('\r\n')
-			print('Received from the Arduino: '+decoded)
-			addAdv =+ int(decoded)
-			x += 1
-		except UnicodeDecodeError:
-			print('Arduino decode error. Ignoring...')
-	totalAdv = addAdv/5
+	global arduinoDistance
+	totalAdv = 0#it will get redefined if the distance sensor runs
+	if arduinoDistance == True:#make a protocal with the arduino
+		while x < 5:#get distance
+			raw = ser.readline()
+			try:#if bits are transmitted incorrectly, it ignores it
+				decoded = raw.decode().strip('\r\n')
+				print('Received from the Arduino: '+decoded)
+				addAdv =+ str(decoded)
+				x += 1
+			except UnicodeDecodeError:
+				print('Arduino decode error. Ignoring...')
+		totalAdv = addAdv/5
 	speedA, speedB = brain(command,totalAdv)#decide what to do with distance and orders
-	pwm = (str(speedA)+'m'+str(speedB))
+	pwm = ('<sd, '+str(speedA)+', '+str(speedB)+'>')
 	pwmmbytes = str.encode(pwm)
 	ser.write(pwmmbytes)#send the data to the arduino
+	ser.flushOutput()
 	print('Motor PWM sent to the Arduino: '+pwm)
 
 def getArduThread():
@@ -161,11 +177,13 @@ def getArduThread():
 		arduino(command)
 
 def runAll():
+	connectArduinoSer()
 	print(' _____     _ _                   _____               ')
 	print('|  |  |_ _| | |_ _ _ ___ ___ ___|_   _|___ ___ ___ __') 
 	print('|  |  | | | |  _| | |  _| -_|___| | | | -_| . |     |')
 	print(' \___/|___|_|_| |___|_| |___|     |_| |___|__,|_|_|_|')
 	print('                    By Jackson Lohman and TJ Reynolds\n')
+	time.sleep(.5)
 	mainMode = inputScrubber('Select an option:\n  (1)Train NN\n  (2)Run NN\n  (3)RC mode\nEnter [1,2,3]: ', ('1','2','3'), 'Invalid Input\n')
 	if mainMode == '1':
 		camTrainDirectionQuestion = 'Select an option:\n  (1) = forward\n  (2) = left\n  (3) = right\n  (4) = stop\n  (5) = other\nEnter [1,2,3,4,5]: '
@@ -186,16 +204,21 @@ def runAll():
 			trainSaveFilepathPrefix = input('Enter a prefix: ')
 		input('Press CTRL-C to quit taking pictures\nPress ENTER to continue\n')
 		print('Starting...')
-		pixels = bluescale()
-		trainImg = Image.fromarray(pixels)
-		if camSupport == True:
-			user = 'pi'
-		else:
-			user = 'jax'
-		trainImg.save('/home/'+user+'/Documents/MLtrain/'+trainSaveFilepathEnd+'/'+trainSaveFilepathPrefix+trainSaveFilepathEnd+str(imgCounter)+'.jpg')
-		#TODO image code
+		global imgCounter
+		while True:
+			pixels = bluescale()
+			trainImg = Image.fromarray(pixels)
+			if camSupport == True:
+				user = 'pi'
+			else:
+				user = 'jax'
+			trainImg.save('/home/'+user+'/Documents/MLtrain/'+trainSaveFilepathEnd+'/'+trainSaveFilepathPrefix+trainSaveFilepathEnd+str(imgCounter)+'.jpg')
+			imgCounter += 1
 	if mainMode == '2':
-		#tf ip addr choice
+		global tfIpAddress
+		tfAddr = input('Is Tensorflow at '+tfIpAddress+'?\n  If so, Press ENTER\n  If not, Enter the correct address :')
+		if tfAddr != '':
+			tfIpAddress = tfAddr
 		threading.Thread(target=getArduThread).start()#thread 1
 		threading.Thread(target=sendCamThread).start()#thread 2
 	if mainMode == '3':
@@ -207,10 +230,9 @@ def runAll():
 			elif RCdirection == 'a':
 				arduino(2)
 			elif RCdirection == 's':
-				arduino(3)
-			elif RCdirection == 'd':
 				arduino(4)
-
+			elif RCdirection == 'd':
+				arduino(3)
 
 runAll()
 
